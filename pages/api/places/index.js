853d543826f71763d4632e580e508b335f69b58f
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-04-23 16:59:11 +0200

    protect POST route in api/places/index.js

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,7 +1,10 @@
 import dbConnect from "../../../db/connect";
 import Place from "../../../db/models/Place";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(request, response) {
+  const session = await getServerSession(request, response, authOptions)
   await dbConnect();
 
   if (request.method === "GET") {
@@ -9,10 +12,14 @@ export default async function handler(request, response) {
     return response.status(200).json(places);
   } else if (request.method === "POST") {
     try {
+      if (session){
       const placeData = request.body;
       const place = new Place(placeData);
       await place.save();
       response.status(201).json({ status: "Place created" });
+      }else{
+        response.status(401).json({ status: "Not authorized" });
+      }
     } catch (error) {
       console.error(error);
       response.status(400).json({ error: error.message });
