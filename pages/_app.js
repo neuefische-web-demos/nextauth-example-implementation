f371ebbc19144c2ec9b2e4622901a6c0e3223352
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-04-24 08:59:20 +0200

    create Auth component to protect the entire app

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,38 @@
 import GlobalStyle from "../styles.js";
 import { SWRConfig } from "swr";
 import Layout from "../components/Layout.js";
+import { SessionProvider, useSession } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: async (...args) => {
-          const response = await fetch(...args);
-          if (!response.ok) {
-            throw new Error(`Request with ${JSON.stringify(args)} failed.`);
-          }
-          return await response.json();
-        },
-      }}
-    >
-      <Layout>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </Layout>
-    </SWRConfig>
+    <SessionProvider session={pageProps.session}>
+      <SWRConfig
+        value={{
+          fetcher: async (...args) => {
+            const response = await fetch(...args);
+            if (!response.ok) {
+              throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+            }
+            return await response.json();
+          },
+        }}
+      >
+        <Layout>
+          <GlobalStyle />
+          <Auth>
+            <Component {...pageProps} />
+          </Auth>
+        </Layout>
+      </SWRConfig>
+    </SessionProvider>
   );
+}
+
+function Auth({ children }) {
+  // required: true makes only 'loading' or 'authenticated' possible. Else the user is redirected to login page.
+  const { status } = useSession({ required: true });
+  if (status === "loading") {
+    return <div>Is loading...</div>;
+  }
+  return children;
 }
